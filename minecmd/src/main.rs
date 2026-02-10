1d90b7d77d96478f5528e5696aeef089ce52f7e6
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2026-02-10 14:11:08 -0800

    New take.

---
+++ minecmd/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    println!("minecmd");
+
+    // build_cmdline();
+    // let x = initialize_database("/Volumes/tmpfs/minecorrosion_test.sqlite3".to_string());
+    println!("break");
+}
